14c0b1d6825816e2afc54c38ba4c4baaaeed8b21
mofblaster <mastero9017@gmail.com>
2019-11-09 20:38:36 +0100

    Fix request and update binary

--- helper.py
+++ helper.py
@@ -9,7 +9,16 @@ BOARDCONFIG = ''
 bloobsPerVersion = 3
 
 if platform.system() == 'Windows':
-    html = urllib.request.urlopen('https://api.ipsw.me/v2.1/' + MODEL + '/latest/version')
+    url = 'https://api.ipsw.me/v2.1/' + MODEL + '/latest/version'
+    request = urllib.request.Request(
+        url,
+        headers={
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                          'Chrome/78.0.3904.97 Safari/537.36 '
+        }
+    )
+
+    html = urllib.request.urlopen(request)
     VERSION = html.read().decode('utf-8')
 
     PATH = MODEL
